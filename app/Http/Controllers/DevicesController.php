327cc71c81ff891ffdbd07921a2ff17556758942
HugoSanchezv <l20030008@fresnillo.tecnm.mx>
2024-08-15 00:41:41 -0600

    restore method setStatusDevice

--- app/Http/Controllers/DevicesController.php
+++ app/Http/Controllers/DevicesController.php
@@ -84,21 +84,15 @@ class DevicesController extends Controller
 
                 ]);
 
-                return redirect()->route('routers.devices', [
-                    'router' => $validatedData['router_id'],
-                    'success' => 'El dispositovo ha sido agreado con éxito'
-                ]);
+                return redirect()->route('routers.devices', ['router' => $router->id])
+                    ->with('success', 'El dispositivo ha sido agregado con éxito');
             } else {
-                return Redirect::route('routers.devices', [
-                    'router' => $validatedData['router_id'],
-                    'error' => 'Error al añadir la dirección, intentalo más tarde'
-                ]);
+                return redirect()->route('routers.devices', ['router' => $router->id])
+                    ->with('error', 'Error al añadir la dirección, inténtalo más tarde');
             }
         } else {
-            return Redirect::route('routers.devices', [
-                'router' => $validatedData['router_id'],
-                'error' => 'Error al intentar conectar con el router, intentalo más tarde'
-            ]);
+            return redirect()->route('routers.devices', ['router' => $router->id])
+                ->with('error', 'Error al intentar conectar con el router, inténtalo más tarde');
         }
     }
 
@@ -133,4 +127,61 @@ class DevicesController extends Controller
     {
         //
     }
+
+
+    public function setDeviceStatus(Device $device)
+    {
+        $device = Device::findOrFail($device->id);
+        $router = $device->router;
+
+        $state = 1;
+        $action = "";
+        $commandAction = "";
+
+
+        if ($device->disabled) {
+            $state = 0;
+            $commandAction = "no";
+            $action = "activar";
+        } else {
+            $state = 1;
+            $commandAction = "yes";
+            $action = "desactivar";
+        }
+
+
+
+        $device->disabled = $state;
+
+        ///////////////////////////////////////////////////////////////////////
+        $router = Router::findOrFail($router->id);
+
+        $ip = $router->ip_address;
+        $user = $router->user;
+        $password = Crypt::decrypt($router->password);
+
+        $API = new RouterosAPI();
+
+        $API->debug(false);
+        //////////////////////////////////////////////////////////////////////////
+
+        //dd($device->device_internal_id);
+        if ($API->connect($ip, $user, $password)) {
+            $API->comm('/ip/firewall/address-list/set', [
+                '.id' => $device->device_internal_id,
+                'disabled' => $commandAction
+            ]);
+
+            $device->disabled = $state;
+            $device->update();
+        } else {
+            $message = 'Falla al ' . $action . ' el dispositivo, intentalo más tarde';
+            return Redirect::route('routers.devices')->with('error', $message);
+        }
+
+        $message = $action . ' ' . $device->address . ' realizado con éxito';
+        return Redirect::route('routers.devices', $router)->with('success', $message);
+    }
+
+    
 }
--- resources/js/Pages/Admin/Routers/DeviceTable.vue
+++ resources/js/Pages/Admin/Routers/DeviceTable.vue
@@ -287,7 +287,7 @@ const confirmSelection = () => {
                   type="checkbox"
                   :checked="cell === 0"
                   class="sr-only peer"
-                  @click="enable(row)"
+                  @click="setDeviceStatus(row)"
                 />
                 <div
                   class="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 peer-focus:ring-blue-300 rounded-full peer bg-gray-300 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-300 peer-checked:bg-green-300"
