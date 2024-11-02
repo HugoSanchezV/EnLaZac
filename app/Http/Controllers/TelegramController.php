f884b5d18cb23cc4cdadc1664a5f269d00d1b9e8
HugoSanchezv <l20030008@fresnillo.tecnm.mx>
2024-11-01 23:48:55 -0600

    add madeline and controller

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class TelegramController extends Controller
 {
