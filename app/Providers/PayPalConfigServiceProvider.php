eaeed0ba78667efb181ef09e51fe04e0d7f41eb8
Juan Pablo Roman <xdylol2002@hotmail.com>
2024-12-27 04:36:14 -0600

    Update PayPalConfigServiceProvider.php

--- app/Providers/PayPalConfigServiceProvider.php
+++ app/Providers/PayPalConfigServiceProvider.php
@@ -23,20 +23,20 @@ class PayPalConfigServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (Schema::hasTable('paypal_accounts')) {
-            $paypalConfig = PaypalAccount::first(); // Obtén el primer registro
+        // if (Schema::hasTable('paypal_accounts')) {
+        //     $paypalConfig = PaypalAccount::first(); // Obtén el primer registro
 
-            if ($paypalConfig) {
-                // Actualizar los valores de la configuración de PayPal
-                Config::set('paypal.sandbox.client_id', $paypalConfig->sandbox_client_id);
-                Config::set('paypal.sandbox.client_secret', $paypalConfig->sandbox_client_secret);
-                Config::set('paypal.live.client_id', $paypalConfig->live_client_id);
-                Config::set('paypal.live.client_secret', $paypalConfig->live_client_secret);
-                Config::set('paypal.mode', $paypalConfig->mode);
-                Config::set('paypal.currency', $paypalConfig->currency);
-            }
-        } else {
-            logger('La tabla paypal_accounts no existe aún.');
-        }
+        //     if ($paypalConfig) {
+        //         // Actualizar los valores de la configuración de PayPal
+        //         Config::set('paypal.sandbox.client_id', $paypalConfig->sandbox_client_id);
+        //         Config::set('paypal.sandbox.client_secret', $paypalConfig->sandbox_client_secret);
+        //         Config::set('paypal.live.client_id', $paypalConfig->live_client_id);
+        //         Config::set('paypal.live.client_secret', $paypalConfig->live_client_secret);
+        //         Config::set('paypal.mode', $paypalConfig->mode);
+        //         Config::set('paypal.currency', $paypalConfig->currency);
+        //     }
+        // } else {
+        //     logger('La tabla paypal_accounts no existe aún.');
+        // }
     }
 }
